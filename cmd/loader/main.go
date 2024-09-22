c6074de40b65733d0d5520313800a888217de3e6
stuart <stuart@workstation.local>
2024-09-22 04:45:57 +0000

    fix: use a single tabwriter

--- cmd/loader/main.go
+++ cmd/loader/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	fname := os.Args[1]
+
 	bin := new(loader.Binary)
 	if err := loader.LoadBinary(fname, bin, loader.BIN_TYPE_AUTO); err != nil {
 		fmt.Printf("could not load binary: %v\n", err)
@@ -19,7 +20,10 @@ func main() {
 
 	fmt.Printf("loaded binary %q %s/%s (%d bits) entry@%#016x\n", bin.Filename, bin.TypeStr, bin.ArchStr, bin.Bits, bin.Entry)
 
+	// we use a tabwriter so that the sections and symbols print out nicely; the
+	// book's C approach doesn't work as well here
 	w := tabwriter.NewWriter(os.Stdout, 0, 1, 1, ' ', tabwriter.TabIndent)
+
 	for _, section := range bin.Sections {
 		fmt.Fprintf(w, "\t%#016x\t%-8d\t%-20s\t%s\t\n", section.Vma, section.Size, section.Name, section.Type)
 	}
@@ -29,7 +33,6 @@ func main() {
 		fmt.Println("scanned symbol tables")
 	}
 
-	w = tabwriter.NewWriter(os.Stdout, 0, 1, 1, ' ', tabwriter.TabIndent)
 	for _, symbol := range bin.Symbols {
 		fmt.Fprintf(w, "\t%-40s\t%#016x\t%s\t\n", symbol.Name, symbol.Addr, symbol.Type)
 	}
